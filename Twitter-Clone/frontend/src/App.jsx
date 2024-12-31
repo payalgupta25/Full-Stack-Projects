6bf3ffa676cd54ff1f0cb3dced5af4b247453797
Payal <payalgupta425@gmail.com>
2024-12-31 12:24:21 +0530

    UI-Design Completed

--- Twitter-Clone/frontend/src/App.jsx
+++ Twitter-Clone/frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';	
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+
 function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
+			{/* this is common as it is used in all pages and is outside the routes */}
+			<Sidebar /> 
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
 			</Routes>
+			<RightPanel />
 		</div>
 	);
 }
